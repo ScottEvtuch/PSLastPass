d0e9c65fec55f20b64cf54c39c03ff68c7a63c64
Scott Evtuch <scott@evtuch.net>
2017-09-07 21:41:44 -0500

    Add basic variables and dependencies

--- PSLastPass.psm1
+++ PSLastPass.psm1
@@ -2,6 +2,48 @@
 
     $ExportParams = @{}
 
+    # Basic variables
+    $LPURL = "https://lastpass.com"
+    $LPUserAgent = "LastPass-CLI/1.2.1"
+
+    # Import the PBKDF2 dll
+    Add-Type -Path  "$PSScriptRoot\bin\PBKDF2.NET.dll" -ErrorAction Stop
+
+    # Set a variable for ASCII encoding
+    $Encoding = [System.Text.Encoding]::GetEncoding("iso-8859-1")
+
+    # Load saved credentials if possible
+    try
+    {
+        $LPCredentials = Import-Clixml -Path "$env:APPDATA\PSLastPass\Creds.xml" -ErrorAction Stop
+    }
+    catch
+    {
+        Write-Verbose "No saved credentials to load, prompting user"
+        $LPCredentials = Get-Credential -Message "Please input your credentials"
+    }
+    
+    # Load saved session if possible
+    try
+    {
+        $LPSession = Import-Clixml -Path "$env:APPDATA\PSLastPass\Session.xml" -ErrorAction Stop
+    }
+    catch
+    {
+        Write-Verbose "No saved session to load"
+        $LPSession = New-Object -TypeName Microsoft.PowerShell.Commands.WebRequestSession
+    }
+
+    # Load saved vault if possible
+    try
+    {
+        $LPVault = Import-Clixml -Path "$env:APPDATA\PSLastPass\Vault.xml" -ErrorAction Stop
+    }
+    catch
+    {
+        Write-Verbose "No saved vault to load"
+    }
+
 #region Public Functions
 
     # Name of the folder for public function ps1 files
